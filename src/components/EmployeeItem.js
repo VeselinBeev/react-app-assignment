701b2f54e77cab449e34c1a521974aa964146bd6
veselin-beev <veselinbeev@gmail.com>
2022-02-07 11:11:15 +0200

    Quick fixes
    Status: Almost Ready

--- src/components/EmployeeItem.js
+++ src/components/EmployeeItem.js
@@ -55,35 +55,25 @@ const EmployeeItem = ({ employee }) => {
 	};
 
 
-	// Hook
+	// Hook to localStorage
 	function useLocalStorage(key, initialValue) {
-		// State to store our value
-		// Pass initial state function to useState so logic is only executed once
 		const [storedValue, setStoredValue] = useState(() => {
 			try {
-				// Get from local storage by key
 				const item = window.localStorage.getItem(key);
-				// Parse stored json or if none return initialValue
 				return item ? JSON.parse(item) : initialValue;
 			} catch (error) {
-				// If error also return initialValue
 				console.log(error);
 				return initialValue;
 			}
 		});
-		// Return a wrapped version of useState's setter function that ...
-		// ... persists the new value to localStorage.
+
 		const setValue = (value) => {
 			try {
-				// Allow value to be a function so we have same API as useState
 				const valueToStore =
 					value instanceof Function ? value(storedValue) : value;
-				// Save state
 				setStoredValue(valueToStore);
-				// Save to local storage
 				window.localStorage.setItem(key, JSON.stringify(valueToStore));
 			} catch (error) {
-				// A more advanced implementation would handle the error case
 				console.log(error);
 			}
 		};
@@ -116,7 +106,9 @@ const EmployeeItem = ({ employee }) => {
 				<div className="list-content">
 					<p className="content-item">
 						<strong>Label:&nbsp;<input type="text" placeholder="Add Label" key={employee.uuid} value={label} onChange={(event) => setLabel(event.target.value)}></input></strong>
-						{/* <div className="list-item">{label}</div> */}
+					</p>
+					<p className="content-item">
+						<strong>Label:&nbsp;</strong><span>{label}</span>
 					</p>
 					<p className="content-item">
 						<strong>Title:&nbsp;</strong>
